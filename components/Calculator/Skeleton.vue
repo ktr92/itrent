e5f3ff2e70089369eaf90ac3046cb82ef2f4cdf3
ktr92 <kudashevtim@gmail.com>
2022-07-14 15:36:27 +0300

    RENT-6 [add] Calculator. skeleton.

---
+++ components/Calculator/Skeleton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="
+      bg-white
+      px-0
+      py-2
+      border-b
+    "
+  >
+    <div class="">
+      <div class="flex items-center mb-2 last:mb-0">
+        <div
+          class="
+            leading-5
+            text-5xs text-black text-opacity-45 text-gray-200
+            bg-gray-200
+            rounded
+            mr-auto
+          "
+        >
+          Площадь помещения
+        </div>
+      </div>
+    </div>
+    <div class="py-2">
+      <div class="flex items-center mb-2 last:mb-0">
+        <div
+          class="
+            leading-5
+            h-12
+            bg-gray-200
+            rounded-lg
+            mr-auto
+            w-full
+          "
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ components/Result/CardSkeleton.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class="
+      bg-white
+      rounded-lg
+      shadow
+      px-10
+      py-2
+      border-3 border-transparent
+    "
+  >
+    <div class="border-b pb-2">
+      <div class="relative">
+        <div
+          class="absolute bg-gray-200 top-0 -left-8 flex w-6 h-6 rounded-full"
+        />
+      </div>
+      <div
+        class="
+          leading-5
+          text-2xs text-black text-opacity-45
+          w-full
+          h-6
+          rounded
+          bg-gray-200
+        "
+      />
+    </div>
+
+    <!--    border-b-->
+    <div class="py-2">
+      <div class="flex items-center mb-2 last:mb-0">
+        <div
+          class="
+            leading-5
+            text-2xs text-black text-opacity-45 text-gray-200
+            bg-gray-200
+            rounded
+            mr-auto
+          "
+        >
+          Цена за кв.м
+        </div>
+        <div
+          class="
+            leading-normal
+            text-2xs text-black text-opacity-85 text-right text-gray-200
+            rounded
+            bg-gray-200
+          "
+        >
+          от 5000 руб / мес
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
