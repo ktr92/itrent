5da2a24352bbefe707a0dd1db0d4123cc7b718c6
Timur Kudashev <kudashev@ipotech.su>
2022-07-21 10:10:56 +0300

    RENT-1 [upd] Calculator. jest tests

--- tests/unit/test.spec.js
+++ tests/unit/test.spec.js
@@ -1,15 +1,11 @@
 
-import { mount, createLocalVue } from '@vue/test-utils'
+import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import calcMutations from '../../store/calculator/mutations'
-import calcActions from '../../store/calculator/actions'
-import calcGetters from '../../store/calculator/getters'
-import calcState from '../../store/calculator/state'
-import Form from '../../components/Calculator/Form'
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-/* test('инициализация значений свойств по умолчанию', () => {
+test('инициализация значений свойств по умолчанию', () => {
   const state = {
     dynamicOptionsParams: [],
     defaultOptions: {
@@ -32,41 +28,4 @@ localVue.use(Vuex)
   expect(state.defaultOptions).not.toHaveProperty('items')
   calcMutations.mergeOptions(state, state.locationOptions)
   expect(state.defaultOptions).toHaveProperty('items')
-}) */
-describe('MyComponent.vue', () => {
-  let actions
-  let getters
-  let state
-  let store
-  beforeEach(() => {
-    actions = {
-      sampleAction: jest.fn()
-    }
-    getters = {
-      sampleGetter: jest.fn()
-    }
-    state = {
-      sampleState: jest.fn()
-    }
-
-    store = new Vuex.Store({
-      modules: {
-        calculator: {
-          namespaced: true,
-          actions: { ...calcActions },
-          getters: { ...calcGetters },
-          mutations: { ...calcMutations },
-          state: { ...calcState }
-        }
-      }
-    })
-  })
-
-  it('вывод свойств по умолчанию', () => {
-    const wrapper = mount(Form, {
-      store,
-      localVue
-    })
-    expect(wrapper.vm.defaultOptions).toHaveLength(0)
-  })
 })
