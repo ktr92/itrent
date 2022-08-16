23875622e6c02cf198df555709eee6ec98eb5709
Timur Kudashev <kudashev@ipotech.su>
2022-08-16 21:17:43 +0300

    RENT-10 [upd] Applications. bpium api. cookie

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,10 +93,6 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ['vee-validate/dist/rules']
-  },
-
-  router: {
-    middleware: ['ssr-cookie']
   }
 
 }
--- store/applications/actions.js
+++ store/applications/actions.js
@@ -4,28 +4,35 @@ export default {
     commit('setMessage', message)
   },
   async sendData ({ commit, rootGetters, dispatch }, payload) {
-    try {
-      const path = 'records'
-      const isAuth = rootGetters['applications/getCookie']
-      const headers = isAuth ? {} : { Authorization: 'Basic ' + base64.encode(`${process.env.BPIUM_LOGIN}` + ':' + `${process.env.BPIUM_PASS}`) }
-      await this.$axios.post(`/api/v1/catalogs/19/${path}`, JSON.stringify({
-        ...payload
-      }),
-      {
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json',
-          ...headers
-        }
-      }).then((response) => {
-        if (response) {
-          commit('changeStep', 2)
-          commit('setCookie', true)
-        }
-      })
-    } catch (e) {
-      console.log(e.response)
-      dispatch('setMessage', { title: `${e.response.status || 'Ошибка'}`, description: `${e.response.data || 'Что-то пошло не так...'}`, type: 'error' })
-    }
+    const path = 'records'
+    const isAuth = rootGetters['applications/getCookie']
+    const headers = isAuth ? {} : { Authorization: 'Basic ' + base64.encode(`${process.env.BPIUM_LOGIN}` + ':' + `${process.env.BPIUM_PASS}`) }
+    await this.$axios.post(`/api/v1/catalogs/19/${path}`, JSON.stringify({
+      ...payload
+    }),
+    {
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+        ...headers
+      }
+    }).then((response) => {
+      if (response) {
+        commit('changeStep', 2)
+        commit('setCookie', true)
+      }
+    }).catch((error) => {
+      if (error.response) {
+        // Запрос был сделан, и сервер ответил кодом состояния, который
+        // выходит за пределы 2xx
+        dispatch('setMessage', { title: `${error.response.status || 'Ошибка'}`, description: `${error.response.data || 'Что-то пошло не так...'}`, type: 'error' })
+      } else if (error.request) {
+        // Запрос был сделан, но ответ не получен
+        dispatch('setMessage', { title: `${'Ошибка'}`, description: `${error.request || 'Что-то пошло не так...'}`, type: 'error' })
+      } else {
+        // Произошло что-то при настройке запроса, вызвавшее ошибку
+        dispatch('setMessage', { title: `${'Ошибка'}`, description: `${error.message || 'Что-то пошло не так...'}`, type: 'error' })
+      }
+    })
   }
 }
